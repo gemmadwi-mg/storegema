0cb449a6bcef78bee4e0fae36cd16672d2f9bd20
GDwiP <gemma.19051@mhs.unesa.ac.id.com>
2022-10-18 08:51:34 +0700

    integrasi voucher detail

--- components/molecules/GameItem/index.tsx
+++ components/molecules/GameItem/index.tsx
@@ -4,13 +4,14 @@ interface GameItemProps {
   title: string;
   category: string;
   thumbnail: string;
+  id: string;
 }
 
 export default function GameItem(props: GameItemProps) {
-  const { title, category, thumbnail } = props;
+  const { title, category, thumbnail, id } = props;
   return (
     <div className="featured-game-card position-relative">
-      <a href="./src/detail.html">
+      <a href={`/detail/${id}`}>
         <div className="blur-sharp">
           <Image
             className="thumbnail"
